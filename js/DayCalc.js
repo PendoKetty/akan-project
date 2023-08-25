406bb7364940eb65b771cd41e9a950e96d512b71
PendoKetty <53566210+PendoKetty@users.noreply.github.com>
2023-08-25 17:07:25 +0300

    Update DayCalc.js

--- js/DayCalc.js
+++ js/DayCalc.js
@@ -1,27 +1,27 @@
 function dayCalc(){
     var year = parseFloat(document.getElementById("year").value);
-    //console.log(year);
+    
   
     var month = parseFloat(document.getElementById("month").value);
-    //console.log(month);
+    
   
     var day = parseFloat(document.getElementById("day").value);
-    //console.log(day);
+    
     
     var century = parseFloat(year/100) +1;
-    //console.log(century);
+   
     
     var birthday = Math.round(( ( (century/4) -2*century-1) + ((5*(year)/4) ) + ((26*(month+1)/10)) + day ) % 7) ;
-    //console.log(birthday);
+    
   
     var gender = document.getElementById("gender").value;
-    //console.log(gender);
+    
   
     var male= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     
     var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa", "Afua","Ama"];
   
-    if(year < 0 || year== null || year== undefined)  {
+    if(year < 0 || year === null || year === undefined)  {
       alert("Invalid Year!");
     }
   
